e85069992fa101233decb47330c24b0983d36e29
YUSAKU UCHIDA <ni34ga@yahoo.co.jp>
2018-12-08 23:14:12 +0900

    make resize function

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -16,5 +16,18 @@ $(function(){
     }
   });
 
-
+  $('input[name^="size"]:radio').change(function(){
+    var radioval = $(this).val();
+    $(this).nextAll('p').text(radioval);
+    if(radioval == 'S') {
+      $(this).parent('span').next('.clip').find('iframe').attr('width', '400');
+      $(this).parent('span').next('.clip').find('iframe').attr('height', '225');
+    }else if(radioval == 'M') {
+      $(this).parent('span').next('.clip').find('iframe').attr('width', '560');
+      $(this).parent('span').next('.clip').find('iframe').attr('height', '315');
+    }else if(radioval == 'L') {
+      $(this).parent('span').next('.clip').find('iframe').attr('width', '720');
+      $(this).parent('span').next('.clip').find('iframe').attr('height', '405');
+    }
+  });
 });
